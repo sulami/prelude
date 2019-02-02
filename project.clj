2fe8ff889bd0035ab94b69986d5734da349dd3ee
Robin Schroer <sulami@peerwire.org>
2019-02-02 18:25:02 +0100

    0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject org.clojars.sulami/prelude "0.0.1"
+(defproject org.clojars.sulami/prelude "0.0.2-SNAPSHOT"
   :license {:name "MIT License"}
   :description "Personal Toolbox"
   :url "https://github.com/sulami/prelude"
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :java-source-paths ["src"]
   :jvm-opts ^:replace ["-server" "-Xmx4g"]
-  :javac-options ["-source" "1.6" "-target" "1.6"]
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
+  :javac-options ["-source" "1.6" "-target" "1.6"])
