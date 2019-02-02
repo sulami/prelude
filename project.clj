9422b9f5a451aaedfe8d08ca96878bf18ca1184c
Robin Schroer <sulami@peerwire.org>
2019-02-02 15:19:06 +0100

    Metadata updates

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.clojars.sulami/prelude "0.0.1"
   :license {:name "MIT License"}
-  :description "Personal play- and dumping ground."
+  :description "Personal Toolbox"
+  :url "https://github.com/sulami/prelude"
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :java-source-paths ["src"]
   :jvm-opts ^:replace ["-server" "-Xmx4g"]
